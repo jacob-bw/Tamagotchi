8b1dae33fcbe87b25ac6bffd2afd1474c138e452
Jacob <wittenberg.jacob@gmail.com>
2019-10-04 23:44:10 -0500

    play quadrant functioning

--- src/javascripts/main.js
+++ src/javascripts/main.js
@@ -1,9 +1,11 @@
 import '../styles/main.scss';
 
 import eat from './components/eat';
+import play from './components/play';
 
 const init = () => {
   eat.eatQuadrant();
+  play.playQuadrant();
 };
 
 init();
