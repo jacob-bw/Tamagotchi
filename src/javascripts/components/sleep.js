69add2bf1ab26e848ea4b9832dedb66033d65ddd
Jacob <wittenberg.jacob@gmail.com>
2019-10-04 23:58:28 -0500

    sleep quadrant functioning

---
+++ src/javascripts/components/sleep.js
@@ -0,0 +1,28 @@
+import utilities from '../helpers/utilities';
+
+const sleepQuadrant = () => {
+  let domString = '';
+  let energy = 100;
+  domString = `
+    <p><progress value="${energy}" max="100" id="sleepTotal"{energy</progress></p>
+    <p><button class='primary' id='deep-sleep'>Deep Sleep</button> <button class ='primary' id='short-nap'>Short Nap</button></p>
+  `;
+  utilities.printToDom(domString, 'sleep');
+  const deepSleepBtn = document.getElementById('deep-sleep');
+  const shortNapBtn = document.getElementById('short-nap');
+
+  deepSleepBtn.addEventListener('click', () => {
+    if (energy < 100) {
+      energy += 60;
+      document.getElementById('sleepTotal').value = energy;
+    }
+  });
+  shortNapBtn.addEventListener('click', () => {
+    if (energy < 100) {
+      energy += 50;
+      document.getElementById('sleepTotal').value = energy;
+    }
+  });
+};
+
+export default { sleepQuadrant };
